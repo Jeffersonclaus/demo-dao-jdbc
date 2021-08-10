10a70d595e1e01aa0234dd6d065af6e4fa8d49ae
Jefferson <jeffersonclaus@gmail.com>
2021-08-10 11:55:27 -0300

    Criar Class Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,10 +11,10 @@ public static void main(String[] args) {
 	
 	
 	Department obj = new Department(1, "book");
-	
+	Seller seller = new Seller(21,"bobo", "bobo@gamil.com", new Date(),2000.00, obj );
 	System.out.println(obj);
-	
-	
+
+	System.out.println(seller);
 }
 
 	
