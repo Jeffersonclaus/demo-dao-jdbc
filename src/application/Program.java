9585778c41d6f68b4e144a0529f91c1347a2e49b
Jefferson <jeffersonclaus@gmail.com>
2021-08-10 13:17:23 -0300

    Implemetando findByid na Class SellerDAOJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,7 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
@@ -10,10 +9,10 @@ public class Program {
 public static void main(String[] args) {
 	
 	
-	Department obj = new Department(1, "book");
-	Seller seller = new Seller(21,"bobo", "bobo@gamil.com", new Date(),2000.00, obj );
-	System.out.println(obj);
-
+SellerDao sellerDao = DaoFactory.creatteSellerDao();
+	
+Seller seller = sellerDao.findByid(3);
+	
 	System.out.println(seller);
 }
 
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -9,7 +9,7 @@ public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
-	Seller findByid(Seller id);
+	Seller findByid(Integer id);
 	List<Seller>findAll();
 	
 }
